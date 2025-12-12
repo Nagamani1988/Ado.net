1bcdb12af4bfa3cd423e72e54f5f94c4a7551696
Nagamani1988 <Nagamani.Kanasani@infinite.com>
2025-12-12 17:52:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentOnAdo.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConnectedArchitecture ob = new ConnectedArchitecture();
+            //ob.DisplayAllCourses();
+
+
+            /*Console.Write("Enter Full Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Enter Department: ");
+            string dept = Console.ReadLine();
+
+            Console.Write("Enter Year Of Study: ");
+            int year = int.Parse(Console.ReadLine());
+
+            ConnectedArchitecture ob = new ConnectedArchitecture();
+            ob.AddStudent(name, email, dept, year);*/
+
+
+            //Console.Write("Enter Department to Search: ");
+           // string dept = Console.ReadLine();
+           // ConnectedArchitecture ob = new ConnectedArchitecture();
+           // ob.SearchStudentsByDepartment(dept);
+
+
+           
+            Console.Write("Enter StudentId: ");
+            int sid = int.Parse(Console.ReadLine());
+            
+            ob.DisplayEnrolledCourses(sid);
+          
+
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+    }
+}
+
+
+
+    
+    
+
