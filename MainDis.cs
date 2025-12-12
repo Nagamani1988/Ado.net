dccf4af8206f87297e80b0a7a30b6d3071ef2124
Nagamani1988 <Nagamani.Kanasani@infinite.com>
2025-12-12 17:52:47 +0530

    Add files via upload

---
+++ MainDis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentOnAdo.net
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisConnectedArchitecture ob = new DisConnectedArchitecture();
+            ob.PrintTable();
+            ob.Credits();
+            ob.Courses();
+            ob.StudentDelete();
+            ob.GetCoueses();
+            Console.ReadLine();
+        }
+
+    }
+}
+
